e435a4816df5732a0b3115017495ff2a0cc4d3c3
KallDrexx <me@mshapiro.net>
2012-11-24 13:36:26 -0500

    Initial commit

---
+++ ILayoutable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FlatRedBall.Graphics;
+using FlatRedBall.Math.Geometry;
+using FlatRedBall.Math;
+using FlatRedBall;
+
+namespace FrbUi
+{
+    public delegate void ILayoutableEvent(ILayoutable sender);
+
+    public interface ILayoutable : IVisible, IScalable, IPositionable
+    {
+        ILayoutableEvent OnSizeChangeHandler { get; set; }
+
+        void AttachTo(PositionedObject obj, bool changeRelative);
+        float RelativeX { get; set; }
+        float RelativeY { get; set; }
+    }
+}

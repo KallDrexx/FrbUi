ff7078b48ddff28e485e7d4e61749e26c254aff2
KallDrexx <me@mshapiro.net>
2013-01-16 19:31:11 -0500

    Added start for debug logging

---
+++ Debug/IUiLogger.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace FrbUi.Debug
+{
+    public interface IUiLogger : IDisposable
+    {
+        void LogMessage(ILayoutable sender, string messageType, string message);
+    }
+}
---
+++ Debug/LogEvents.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FrbUi.Debug
+{
+    public static class LogEvents
+    {
+        public const string All = "all";
+        public const string ItemAddedToLayout = "ItemAddedToLayout";
+        public const string SelectableFocused = "SelectableFocused";
+    }
+}

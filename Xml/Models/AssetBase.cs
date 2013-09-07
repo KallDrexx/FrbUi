49d6b0a99a988830fcb511af3abc5629fe2e0d55
KallDrexx <me@mshapiro.net>
2013-09-07 11:44:53 -0400

    Added some examples of optional xml attributes

--- Xml/Models/AssetBase.cs
+++ Xml/Models/AssetBase.cs
@@ -8,14 +8,70 @@ namespace FrbUi.Xml.Models
 {
     public abstract class AssetBase
     {
-        [XmlAttribute] public string Name { get; set; }
-        [XmlAttribute] public float? RelativeX { get; set; }
-        [XmlAttribute] public float RelativeY { get; set; }
-        [XmlAttribute] public float RelativeZ { get; set; }
-        [XmlAttribute] public float ScaleX { get; set; }
-        [XmlAttribute] public float ScaleY { get; set; }
-        [XmlAttribute] public bool Visible { get; set; }
+        #region XML Values
 
+        [XmlAttribute]
+        public string Name { get; set; }
+
+        [XmlAttribute]
+        public float ScaleX { get; set; }
+
+        [XmlAttribute]
+        public float ScaleY { get; set; }
+
+        [XmlAttribute]
+        public bool Visible { get; set; }
+
+        [XmlIgnore]
+        public float? RelativeX { get; set; }
+
+        [XmlIgnore]
+        public bool RelativeXValueSpecified
+        {
+            get { return RelativeX.HasValue; }
+        }
+
+        [XmlIgnore]
+        public float? RelativeY { get; set; }
+
+        [XmlIgnore]
+        public bool RelativeYValueSpecified
+        {
+            get { return RelativeY.HasValue; }
+        }
+
+        [XmlIgnore]
+        public float? RelativeZ { get; set; }
+
+        [XmlIgnore]
+        public bool RelativeZValueSpecified
+        {
+            get { return RelativeZ.HasValue; }
+        }
+
+        [XmlAttribute("RelativeX")]
+        public float RelativeXValue
+        {
+            get { return RelativeX ?? default(float); }
+            set { RelativeX = value; }
+        }
+
+        [XmlAttribute("RelativeY")]
+        public float RelativeYValue
+        {
+            get { return RelativeY ?? default(float); }
+            set { RelativeY = value; }
+        }
+
+        [XmlAttribute("RelativeZ")]
+        public float RelativeZValue
+        {
+            get { return RelativeZ ?? default(float); }
+            set { RelativeZ = value; }
+        }
+
+        #endregion
+        
         protected AssetBase()
         {
             Visible = true;
@@ -34,6 +90,9 @@ namespace FrbUi.Xml.Models
             layoutable.ScaleX = ScaleX;
             layoutable.ScaleY = ScaleY;
             layoutable.Visible = Visible;
+            layoutable.RelativeX = RelativeXValue;
+            layoutable.RelativeY = RelativeYValue;
+            layoutable.RelativeZ = RelativeZValue;
 
             if (!string.IsNullOrWhiteSpace(Name))
             {

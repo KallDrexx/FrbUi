c71cbf734efb87d62c0a49fcd5f6cb671d321efd
KallDrexx <me@mshapiro.net>
2013-10-02 22:17:13 -0400

    Added FrbUiSettings to get details about FrbUi, such as the version

---
+++ FrbUiSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace FrbUi
+{
+    public static class FrbUiSettings
+    {
+        public static Version Version
+        {
+            get
+            {
+                return Assembly.GetAssembly(typeof (FrbUiSettings))
+                               .GetName()
+                               .Version;
+            }
+        }
+    }
+}
